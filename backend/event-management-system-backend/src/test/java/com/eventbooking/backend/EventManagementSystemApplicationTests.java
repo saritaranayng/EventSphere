83caf3dfba106318111169eb2e042e719fbc820e
ldtsarita <ldtsarita@gmail.com>
2025-11-18 19:03:49 +0530

    Setup done

---
+++ backend/event-management-system-backend/src/test/java/com/eventbooking/backend/EventManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventbooking.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
